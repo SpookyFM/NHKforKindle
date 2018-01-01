a4c4130d5139355d61919ac7701c2e5079483d10
Florian Mehm <florian@mehm.net>
2018-01-01 20:31:09 +0100

    Changed the script to download a fixed number of news or up until the latest synced news item

--- nhk-today.py
+++ nhk-today.py
@@ -7,19 +7,46 @@ import re
 from bs4 import BeautifulSoup
 from datetime import date
 
-def main():
 
+# If this is the first run, we will load this number of news
+numInitialNews = 5
+
+# Latest news id that was exported during the last run
+latestNewsId = None
+
+# List in which all news to export are gathered
+items = []
+
+# Content of the news to be exported
+content = []
+    
+
+def main():
+    global latestNewsId
+    latestNewsId = getLastNews()
     r = requests.get('http://www3.nhk.or.jp/news/easy/news-list.json')
     r.encoding = 'utf-8-sig'
     o = json.loads(r.text)
     parse(o)
-
+    writeResult(date.today().strftime("%Y-%m-%d"))
+    
+# Returns the last news read or None if no news have been read so far
+def getLastNews():
+    if (os.path.isfile(".latest") == True):
+        # If we have a .latest-file, read the last id from item
+        file = open(".latest", "r") 
+        return file.read()
+    else:
+        return None
+    
 def parse(o):
     for k, v in o[0].items():
-        if k == date.today().strftime("%Y-%m-%d"):
-            parseDate(k, v)
-
-def parseDate(date, news):
+        shouldBreak = parseDate(k, v)
+        if (shouldBreak):
+            break
+    
+def writeResult(date):
+    global latestNewsId
     output = date + '.html'
     folder = 'data/' + date
 
@@ -29,15 +56,8 @@ def parseDate(date, news):
  #   elif os.listdir(folder):
  #       print("Directory \"" + folder + "/\" exists!\n\nAbort.\n")
  #       return
-
-    items = []
-    content = []
-    for i in news:
-        item = parseNews(i);
-        items.append(item)
-        content.append(item["content"])
-
-    with open(output, "w") as f:
+    
+    with codecs.open(output, "w", "utf-8-sig") as f:
         print('<?xml version="1.0" encoding="UTF-8" ?>', file=f)
         print("<!DOCTYPE html>", file=f)
         print("<html lang='ja'>", file=f)
@@ -50,7 +70,7 @@ def parseDate(date, news):
         print("</html>", file=f)
         print("File \"" + output + "\" created")
         
-        file = open(date + '.opf', "w")
+        file = codecs.open(date + '.opf', "w", "utf-8-sig")
         file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><package version=\"3.0\" xmlns=\"http://www.idpf.org/2007/opf\"         unique-identifier=\"BookId\"> <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\"           xmlns:dcterms=\"http://purl.org/dc/terms/\">   <dc:title>NHK ニュース・読み物・" + date + "</dc:title>    <dc:contributor>NHK</dc:contributor>   <dc:language>ja</dc:language>   <dc:publisher>NHK</dc:publisher> </metadata> <manifest>  <item id=\"titlepage\" href=\"" + output + "\" media-type=\"application/xhtml+xml\" /> </manifest> <spine toc=\"tocncx\" page-progression-direction=\"rtl\">  <itemref idref=\"titlepage\" /> </spine></package>")
         file.close()
         
@@ -63,8 +83,40 @@ def parseDate(date, news):
             f.write(r.content)
             print("File \"" + i["voice"]["file"] + "\" downloaded")
 
+    # Save the info of the latest synced news id
+    with open(".latest", "w") as f:
+        if (len(items) == 0):
+            f.write(latestNewsId)
+        else:
+            f.write(items[0]["id"])
+        
     print("Today news were downloaded from NHK.")
+    
+    
+# Parses the news on that date. Returns True if the loop should break at this point. Writes all news to export to the items and content lists
+def parseDate(date, news):
+    global latestNewsId
+    for i in news:
+        item = parseNews(i);
+        shouldAdd = False
+        if (latestNewsId == None):
+            # We need to limit ourselves to numInitialNews
+            shouldAdd = len(items) < numInitialNews
+        else:
+            # We need to sync up until latestNewsId
+            print (latestNewsId + " " + item["id"])
+            shouldAdd = item["id"] != latestNewsId
+        if (shouldAdd == True):
+            items.append(item)
+            content.append(item["content"])
+        else:
+            # As soon as we don't add something anymore, we are done
+            return True
+            
+    return False
 
+    
+# Parses a single news entry
 def parseNews(news):
     news_id = news['news_id']
     news_time = news['news_prearranged_time'].replace(':', '-')
@@ -101,7 +153,8 @@ def parseNews(news):
 
     return {
         "content": str(title) + str(article),
-        "voice": voice
+        "voice": voice,
+        "id": news_id
     }
 
 main()

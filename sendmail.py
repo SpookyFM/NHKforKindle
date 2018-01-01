38c19e919be58769fe477298427e908e0b3c8509
Florian Mehm <florian@mehm.net>
2018-01-01 21:27:33 +0100

    Fixes for sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -2,35 +2,35 @@ from datetime import date
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.utils import formatdate
+import os
 
 def main():
-
-	# Adjust these lines for your mail server setup
-	smtpServer = "YOUR SMTP SERVER HERE"
-	smtpPort = 587
-	smtpUser = "YOUR SMTP USER HERE"
-	smtpPassword = "YOUR SMTP PASSWORD HERE"
-	messageFrom = "SENDER MAIL ADDRESS"
-	messageTo = "RECIPIENT MAIL ADDRESS"
-	
-# Check if there is a file for today
-	todayFilename = date.today().strftime("%Y-%m-%d") + ".mobi"
-	if (os.path.isfile(todayFilename) == True):
-		# If so, send it
-		sendMail()
+    # Check if there is a file for today
+    todayFilename = date.today().strftime("%Y-%m-%d") + ".mobi"
+    if (os.path.isfile(todayFilename) == True):
+        # If so, send it
+        sendMail()
 
 def sendMail():
-        server = smtplib.SMTP(smtpServer, smtpPort)
+    # Adjust these lines for your mail server setup
+    smtpServer = "YOUR SMTP SERVER HERE"
+    smtpPort = 587
+    smtpUser = "YOUR SMTP USER HERE"
+    smtpPassword = "YOUR SMTP PASSWORD HERE"
+    messageFrom = "SENDER MAIL ADDRESS"
+    messageTo = "RECIPIENT MAIL ADDRESS"
 
-        #Next, log in to the server
-        server.login(smtpUser, smtpPassword)
+    server = smtplib.SMTP(smtpServer, smtpPort)
 
-#Send the mail
-        msg = MIMEMultipart()
-        msg['From'] = messageFrom
-        msg['To'] = messageTo
-        msg['Date']= formatdate(localtime = True)
-        msg['Subject'] = "NHK News"
-        server.sendmail(messageFrom, messageTo, msg.as_string())
+    #Next, log in to the server
+    server.login(smtpUser, smtpPassword)
+
+    #Send the mail
+    msg = MIMEMultipart()
+    msg['From'] = messageFrom
+    msg['To'] = messageTo
+    msg['Date']= formatdate(localtime = True)
+    msg['Subject'] = "NHK News"
+    server.sendmail(messageFrom, messageTo, msg.as_string())
 
 main()
